15ae9581a5b3e218f4f12732c7d8a5dfc9151130
Ivan Lev <fanatos@mail.ru>
2023-11-29 17:37:55 +0500

    feat: add content to main section

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,12 +1,59 @@
 import React from 'react';
 import './Main.scss';
 
+import avatar from '../../images/avatar.jpg';
+import sass from '../../images/icon-tech-sass.svg';
+
 function Main() {
   return (
     <main className="main">
-      <span>Main</span>
-      <span>content</span>
-      <span>here</span>
+      <section className="main__hero">
+        <div className="main__hero-text">
+          Hi 👋,
+          <br />
+          My name is
+          <br />
+          <span className="main__hero-name">Ivan Lev</span>
+          <br />I build things for web
+        </div>
+        <img className="main__avatar" src={avatar} alt="My photo"></img>
+      </section>
+      <section className="main__stack">
+        <h2 className="main__stack-title">My Tech Stack</h2>
+        <p className="main__stack-subtitle"> Technologies I’ve been working with recently</p>
+        <ul className="main__stack-list">
+          <li>
+            <img src={sass}></img>
+          </li>
+          <li>
+            <img src={sass}></img>
+          </li>
+          <li>
+            <img src={sass}></img>
+          </li>
+          <li>
+            <img src={sass}></img>
+          </li>
+          <li>
+            <img src={sass}></img>
+          </li>
+          <li>
+            <img src={sass}></img>
+          </li>
+          <li>
+            <img src={sass}></img>
+          </li>
+          <li>
+            <img src={sass}></img>
+          </li>
+          <li>
+            <img src={sass}></img>
+          </li>
+          <li>
+            <img src={sass}></img>
+          </li>
+        </ul>
+      </section>
     </main>
   );
 }
