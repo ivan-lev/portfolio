95356b49cf0ee322b7bd4296b7fdd9710f67c536
Ivan Lev <fanatos@mail.ru>
2023-11-29 17:35:30 +0500

    feat: add template link for this project

--- src/components/Credits/Credits.jsx
+++ src/components/Credits/Credits.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Credits.scss';
 
 function Credits() {
   return (
     <>
-      <a href="https://www.flaticon.com/free-icon/briefcase_3324799">Icon from Freepik</a>
+      <Link to="https://www.flaticon.com/free-icon/briefcase_3324799">Icon from Freepik</Link>
+      <Link to="https://www.figma.com/file/WrsiAQStgOWMzBWLzPHDAi/Developer-Portfolio-Design-(Community)?type=design&t=hRTNcG4J9JY8YIR6-6">
+        Portfolio template
+      </Link>
     </>
   );
 }
