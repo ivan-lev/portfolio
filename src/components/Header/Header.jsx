25e0aa62fe48be9d3e6bac7912616a20ad2b4cdf
Ivan Lev <fanatos@mail.ru>
2023-11-29 16:59:46 +0500

    feat: add header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,53 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.scss';
+import logo from '../../images/favicon.png';
+import iconGithub from '../../images/icon-social-github.svg';
+import iconLinkedIn from '../../images/icon-social-linkedin.svg';
 
 function Header() {
   return (
-    <header>
-      <span>Header</span>
-      <span>here</span>
+    <header className="header">
+      <Link to="/">
+        <img className="header__logo" src={logo}></img>
+      </Link>
+      <menu className="header__content">
+        <li>
+          <Link className="header__link" to="#">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link className="header__link" to="#">
+            About
+          </Link>
+        </li>
+        <li>
+          <Link className="header__link" to="#">
+            Tech Stack
+          </Link>
+        </li>
+        <li>
+          <Link className="header__link" to="#">
+            Projects
+          </Link>
+        </li>
+        <li>
+          <Link className="header__link" to="#">
+            Contact
+          </Link>
+        </li>
+        <li>
+          <Link to="#">
+            <img className="header__social-logo" src={iconGithub}></img>
+          </Link>
+        </li>
+        <li>
+          <Link to="#">
+            <img className="header__social-logo" src={iconLinkedIn}></img>
+          </Link>
+        </li>
+      </menu>
     </header>
   );
 }
