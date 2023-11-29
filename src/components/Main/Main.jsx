87e25f331029fa296ed55bb3f58dc7c4c83d7875
Ivan Lev <fanatos@mail.ru>
2023-11-29 17:52:57 +0500

    feat: add tech icons to main section

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,6 +3,14 @@ import './Main.scss';
 
 import avatar from '../../images/avatar.jpg';
 import sass from '../../images/icon-tech-sass.svg';
+import css from '../../images/icon-tech-css.svg';
+import git from '../../images/icon-tech-git.svg';
+import github from '../../images/icon-tech-github.svg';
+import html from '../../images/icon-tech-html.svg';
+import js from '../../images/icon-tech-js.svg';
+import nodejs from '../../images/icon-tech-nodejs.svg';
+import react from '../../images/icon-tech-react.svg';
+import redux from '../../images/icon-tech-redux.svg';
 
 function Main() {
   return (
@@ -23,34 +31,31 @@ function Main() {
         <p className="main__stack-subtitle"> Technologies I’ve been working with recently</p>
         <ul className="main__stack-list">
           <li>
-            <img src={sass}></img>
+            <img className="main__stack-list-element" src={html}></img>
           </li>
           <li>
-            <img src={sass}></img>
+            <img className="main__stack-list-element" src={css}></img>
           </li>
           <li>
-            <img src={sass}></img>
+            <img className="main__stack-list-element" src={sass}></img>
           </li>
           <li>
-            <img src={sass}></img>
+            <img className="main__stack-list-element" src={git}></img>
           </li>
           <li>
-            <img src={sass}></img>
+            <img className="main__stack-list-element" src={github}></img>
           </li>
           <li>
-            <img src={sass}></img>
+            <img className="main__stack-list-element" src={js}></img>
           </li>
           <li>
-            <img src={sass}></img>
+            <img className="main__stack-list-element" src={nodejs}></img>
           </li>
           <li>
-            <img src={sass}></img>
+            <img className="main__stack-list-element" src={react}></img>
           </li>
           <li>
-            <img src={sass}></img>
-          </li>
-          <li>
-            <img src={sass}></img>
+            <img className="main__stack-list-element" src={redux}></img>
           </li>
         </ul>
       </section>
