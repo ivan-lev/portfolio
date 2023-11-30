7c469fa736419b5a2117502cd5896e497c85955a
Ivan Lev <fanatos@mail.ru>
2023-11-30 13:44:57 +0500

    style: replace html code with Menu component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.scss';
+
+import Menu from '../Menu/Menu.jsx';
 import logo from '../../images/favicon.png';
 import iconGithub from '../../images/icon-social-github.svg';
 import iconLinkedIn from '../../images/icon-social-linkedin.svg';
@@ -11,32 +13,8 @@ function Header() {
       <Link to="/">
         <img className="header__logo" src={logo}></img>
       </Link>
-      <menu className="header__content">
-        <li>
-          <Link className="header__link" to="#">
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link className="header__link" to="#">
-            About
-          </Link>
-        </li>
-        <li>
-          <Link className="header__link" to="#">
-            Tech Stack
-          </Link>
-        </li>
-        <li>
-          <Link className="header__link" to="#">
-            Projects
-          </Link>
-        </li>
-        <li>
-          <Link className="header__link" to="#">
-            Contact
-          </Link>
-        </li>
+      <div className="header__content">
+        <Menu />
         <li>
           <Link to="#">
             <img className="header__social-logo" src={iconGithub}></img>
@@ -47,7 +25,7 @@ function Header() {
             <img className="header__social-logo" src={iconLinkedIn}></img>
           </Link>
         </li>
-      </menu>
+      </div>
     </header>
   );
 }
