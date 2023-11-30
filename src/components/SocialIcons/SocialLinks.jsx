9f02c4110295f94d535f305f27cd1b21aea36cf0
Ivan Lev <fanatos@mail.ru>
2023-11-30 13:51:59 +0500

    feat: add SocialLinks component

---
+++ src/components/SocialIcons/SocialLinks.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './SocialLinks.scss';
+
+import iconGithub from '../../images/icon-social-github.svg';
+import iconLinkedIn from '../../images/icon-social-linkedin.svg';
+
+export default function SocialIcons() {
+  return (
+    <>
+      <Link to="#">
+        <img className="social-icon" src={iconGithub}></img>
+      </Link>
+      <Link to="#">
+        <img className="social-icon" src={iconLinkedIn}></img>
+      </Link>
+    </>
+  );
+}
