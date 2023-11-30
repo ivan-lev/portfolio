2d5da9e53800893793d562a6bc073c4e6bf6794a
Ivan Lev <fanatos@mail.ru>
2023-11-30 13:52:19 +0500

    style: replace html with SocialLinks component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 import './Header.scss';
 
 import Menu from '../Menu/Menu.jsx';
+import SocialLinks from '../SocialIcons/SocialLinks.jsx';
+
 import logo from '../../images/favicon.png';
-import iconGithub from '../../images/icon-social-github.svg';
-import iconLinkedIn from '../../images/icon-social-linkedin.svg';
 
 function Header() {
   return (
@@ -15,16 +15,7 @@ function Header() {
       </Link>
       <div className="header__content">
         <Menu />
-        <li>
-          <Link to="#">
-            <img className="header__social-logo" src={iconGithub}></img>
-          </Link>
-        </li>
-        <li>
-          <Link to="#">
-            <img className="header__social-logo" src={iconLinkedIn}></img>
-          </Link>
-        </li>
+        <SocialLinks />
       </div>
     </header>
   );
