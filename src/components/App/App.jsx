8ee7e7d3f7587e071a6d33f2f918e8bfec4932e7
Ivan Lev <fanatos@mail.ru>
2023-11-30 17:04:07 +0500

    feat: add Routes to App component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.scss';
 
 import Header from '../Header/Header.jsx';
 import Main from '../Main/Main.jsx';
 import Footer from '../Footer/Footer.jsx';
 
+import About from '../About/About.jsx';
+
 function App() {
   return (
     <div className="page">
       <Header />
-      <Main />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="about" element={<About />} />
+      </Routes>
       <Footer />
     </div>
   );
