b8ff84c01ff15c530266f65648a839592e682689
Ivan Lev <fanatos@mail.ru>
2023-11-30 13:44:26 +0500

    feat: add Menu component

---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Menu.scss';
+
+export default function Menu() {
+  return (
+    <menu className="menu__content">
+      <li>
+        <Link className="menu__link" to="#">
+          Home
+        </Link>
+      </li>
+      <li>
+        <Link className="menu__link" to="#">
+          About
+        </Link>
+      </li>
+      <li>
+        <Link className="menu__link" to="#">
+          Tech Stack
+        </Link>
+      </li>
+      <li>
+        <Link className="menu__link" to="#">
+          Projects
+        </Link>
+      </li>
+      <li>
+        <Link className="menu__link" to="#">
+          Contact
+        </Link>
+      </li>
+    </menu>
+  );
+}
