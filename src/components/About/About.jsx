f81f41fb72d4426709635df506848474752d808c
Ivan Lev <fanatos@mail.ru>
2023-12-01 12:16:24 +0500

    feat: add initial About component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,5 +2,16 @@ import React from 'react';
 import './About.scss';
 
 export default function About() {
-  return 'some text here';
+  return (
+    <section>
+      <h2 className="main__title">About Me</h2>
+      <p className="main__text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia iste blanditiis, explicabo
+        sapiente molestias et sint facilis officiis aperiam nihil nesciunt aut similique. Illum,
+        tempore facere. Adipisci nesciunt nostrum repudiandae?
+      </p>
+      <h2 className="main__title">Work Experience</h2>
+      <h2 className="main__title">Education</h2>
+    </section>
+  );
 }
