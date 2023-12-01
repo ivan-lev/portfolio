0b6a14d91d76ee6d7ffd963ee433bf4ac78b3fc7
Ivan Lev <fanatos@mail.ru>
2023-12-01 16:27:21 +0500

    feat: make good Credits component

--- src/components/Credits/Credits.jsx
+++ src/components/Credits/Credits.jsx
@@ -2,13 +2,40 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Credits.scss';
 
-function Credits() {
+import logo from '../../images/favicon.png';
+import telegramIcon from '../../images/icon-social-telegram.svg';
+
+export default function Credits() {
   return (
-    <>
-      <Link to="https://www.flaticon.com/free-icon/briefcase_3324799">Icon from Freepik</Link>
-      <Link to="https://www.figma.com/file/WrsiAQStgOWMzBWLzPHDAi/Developer-Portfolio-Design-(Community)?type=design&t=hRTNcG4J9JY8YIR6-6">
-        Portfolio template
-      </Link>
-    </>
+    <section>
+      <ul className="credits__list">
+        <li className="credits__list-element">
+          <span className="main__text">This site was created using free&nbsp;</span>
+          <Link
+            className="credits__link"
+            to="https://www.figma.com/file/WrsiAQStgOWMzBWLzPHDAi/Developer-Portfolio-Design-(Community)?type=design&t=hRTNcG4J9JY8YIR6-6"
+          >
+            template from Figma
+          </Link>
+        </li>
+        <li className="credits__list-element">
+          <span className="main__text">Logo was founded on&nbsp;</span>
+          <Link className="credits__link" to="https://www.flaticon.com/search?word=portfolio">
+            Flaticon.com
+          </Link>
+          <img className="credits__image" src={logo}></img>
+        </li>
+        <li className="credits__list-element">
+          <span className="main__text">Telegram icon from&nbsp;</span>
+          <Link
+            className="credits__link"
+            to="https://icons8.com/icon/QP6ADhc43z2T/telegram#:~:text=look%20like%20this%3A-,Telegram,-icon%20by%20Icons8"
+          >
+            Freepik.com
+          </Link>
+          <img className="credits__image" src={telegramIcon}></img>
+        </li>
+      </ul>
+    </section>
   );
 }
