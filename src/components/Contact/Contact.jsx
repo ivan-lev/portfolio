5cee42ba28346769fb5143157a67a7e9805816f5
Ivan Lev <fanatos@mail.ru>
2023-12-01 15:11:25 +0500

    feat: add Contact component

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Contact.scss';
+
+export default function Contact() {
+  return (
+    <section>
+      <h2 className="main__title">For any questions please mail me:</h2>
+      <a className="about__email" href="mailto:fanatos@mail.ru">
+        fanatos@mail.ru
+      </a>
+    </section>
+  );
+}
