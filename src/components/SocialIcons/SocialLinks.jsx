2e0333f852afae73c024ce9a0c34fc6ad7cf52b2
Ivan Lev <fanatos@mail.ru>
2023-12-01 16:26:05 +0500

    feat: add Telegram link to SocialLinks component

--- src/components/SocialIcons/SocialLinks.jsx
+++ src/components/SocialIcons/SocialLinks.jsx
@@ -3,17 +3,21 @@ import { Link } from 'react-router-dom';
 
 import './SocialLinks.scss';
 
-import iconGithub from '../../images/icon-social-github.svg';
-import iconLinkedIn from '../../images/icon-social-linkedin.svg';
+import githubIcon from '../../images/icon-social-github.svg';
+import linkedInIcon from '../../images/icon-social-linkedin.svg';
+import telegramIcon from '../../images/icon-social-telegram.svg';
 
 export default function SocialLinks() {
   return (
     <div className="social-links">
       <Link to="https://github.com/ivan-lev">
-        <img className="social-icon" src={iconGithub}></img>
+        <img className="social-icon" src={githubIcon}></img>
       </Link>
       <Link to="#">
-        <img className="social-icon" src={iconLinkedIn}></img>
+        <img className="social-icon" src={linkedInIcon}></img>
+      </Link>
+      <Link to="https://t.me/ivanlev">
+        <img className="social-icon" src={telegramIcon}></img>
       </Link>
     </div>
   );
