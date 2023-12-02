472913ddbf999746eeab0552102f4918fe209a56
Ivan Lev <fanatos@mail.ru>
2023-12-03 00:32:42 +0500

    feat: add language context and functionality

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.scss';
+
+import { LanguageContext, languages } from '../../contexts/languageContext.js';
 
 import Header from '../Header/Header.jsx';
 import Main from '../Main/Main.jsx';
@@ -10,16 +12,43 @@ import Contact from '../Contact/Contact.jsx';
 import Credits from '../Credits/Credits.jsx';
 
 function App() {
+  const localStoredLang = localStorage.getItem('lang');
+  //const localStoredColorTheme = JSON.parse(localStorage.getItem('currentTheme'));
+  const [language, setLanguage] = useState('localStoredLang');
+
+  useEffect(() => {
+    if (!localStorage.getItem('lang')) {
+      localStorage.setItem('lang', 'en');
+    }
+    setLanguage(languages[localStoredLang]);
+  }, []);
+
+  const toggleLanguage = () => {
+    if (localStoredLang === 'en') {
+      localStorage.setItem('lang', 'ru');
+      setLanguage(languages['ru']);
+    }
+    if (localStoredLang === 'ru') {
+      localStorage.setItem('lang', 'en');
+      setLanguage(languages['en']);
+    }
+  };
+
   return (
     <div className="page">
-      <Header />
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/credits" element={<Credits />} />
-      </Routes>
-      <Footer />
+      <button className="toggle-language-button" onClick={toggleLanguage}>
+        ru-en
+      </button>
+      <LanguageContext.Provider value={language}>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/credits" element={<Credits />} />
+        </Routes>
+        <Footer />
+      </LanguageContext.Provider>
     </div>
   );
 }
