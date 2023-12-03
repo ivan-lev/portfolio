92ac77c5d2e6ce82c81e6e0087f3ee1b235ebb2a
Ivan Lev <fanatos@mail.ru>
2023-12-03 22:47:29 +0500

    fix: update themization function

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -37,12 +37,23 @@ function App() {
   const toggleTheme = () => {
     const elem = document.documentElement;
     const computedStyles = getComputedStyle(elem);
-    const bgColor = computedStyles.getPropertyValue('--color-dark');
-    if (bgColor === '#191919') {
-      elem.style.setProperty('--color-dark', '#333333');
+    const bgColor = computedStyles.getPropertyValue('--bg-color-primary');
+    //console.log(bgColor);
+    if (bgColor === '#faf0e6') {
+      elem.style.setProperty('--bg-color-primary', 'var(--bg-dark-main)');
+      elem.style.setProperty('--bg-color-secondary', 'var(--bg-dark-card');
+      elem.style.setProperty('--color-hero', 'var(--color-dark-hero)');
+      elem.style.setProperty('--color-primary', 'var(--color-dark-primary)');
+      elem.style.setProperty('--color-secondary', 'var(--color-dark-secondary)');
+      elem.style.setProperty('--color-additional', 'var(--color-dark-additional)');
     }
-    if (bgColor === '#333333') {
-      elem.style.setProperty('--color-dark', '#191919');
+    if (bgColor === '#191919') {
+      elem.style.setProperty('--bg-color-primary', 'var(--bg-beige-main)');
+      elem.style.setProperty('--bg-color-secondary', 'var(--bg-beige-card');
+      elem.style.setProperty('--color-hero', 'var(--color-beige-hero)');
+      elem.style.setProperty('--color-primary', 'var(--color-beige-primary)');
+      elem.style.setProperty('--color-secondary', 'var(--color-beige-secondary)');
+      elem.style.setProperty('--color-additional', 'var(--color-beige-additional)');
     }
   };
 
