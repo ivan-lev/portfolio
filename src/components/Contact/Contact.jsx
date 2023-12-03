99b3c04a79dd7b5822b992f62c1d28b882f541ee
Ivan Lev <fanatos@mail.ru>
2023-12-04 01:09:24 +0500

    feat: add language context to Contact component

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './Contact.scss';
 
+import { LanguageContext } from '../../contexts/languageContext.js';
+
 export default function Contact() {
+  const language = useContext(LanguageContext);
+
   return (
     <section>
-      <h2 className="main__title">For any questions please mail me:</h2>
+      <h2 className="main__title">{language.contactText}</h2>
       <a className="about__email" href="mailto:fanatos@mail.ru">
         fanatos@mail.ru
       </a>
