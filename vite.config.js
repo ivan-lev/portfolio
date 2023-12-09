884e9cb630994ae257ab9fab8a9577b7060975cb
Ivan Lev <fanatos@mail.ru>
2023-12-09 18:56:45 +0500

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/portfolio/',
   plugins: [react(), svgr()],
   build: {
     rollupOptions: {
