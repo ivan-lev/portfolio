533b6aa03460ee0fe0f14f54dc0f656aa891a07e
Ivan Lev <fanatos@mail.ru>
2023-12-09 17:25:46 +0500

    feat: add settings for build

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,28 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()]
+  base: './',
+  plugins: [react(), svgr()],
+  build: {
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+
+        assetFileNames: ({ name }) => {
+          if (/\.(gif|jpe?g|png|svg)$/.test(name ?? '')) {
+            return 'assets/images/[name]-[hash][extname]';
+          }
+
+          if (/\.css$/.test(name ?? '')) {
+            return 'assets/css/[name]-[hash][extname]';
+          }
+
+          // default value
+          // ref: https://rollupjs.org/guide/en/#outputassetfilenames
+          return 'assets/[name]-[hash][extname]';
+        }
+      }
+    }
+  }
 });
