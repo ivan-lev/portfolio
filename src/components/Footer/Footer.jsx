4d1832c080354c84f51f2d22d2cd903ecd9a5549
Ivan Lev <fanatos@mail.ru>
2023-12-10 10:11:01 +0500

    style: change footer look and styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -12,18 +12,11 @@ function Footer() {
 
   return (
     <footer className="footer">
-      <div className="footer__top">
-        <Logo />
-        <div className="footer__top-content">
-          <a href="tel:+79030909030">+7-903-090-90-30</a>
-          <a href="mailto:fanatos@mail.ru">fanatos@mail.ru</a>
-          <SocialIcons />
-        </div>
-      </div>
       <div className="footer__divider" />
-      <div className="footer__bottom">
-        <Menu />
-        <div className="footer__bottom-credits">{translation?.footerText}</div>
+      <span className="footer__credits">{translation?.footerText}</span>
+      <div className="footer__info">
+        <a href="tel:+79030909030">+7-903-090-90-30</a>
+        <a href="mailto:fanatos@mail.ru">fanatos@mail.ru</a>
       </div>
     </footer>
   );
