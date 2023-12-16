bedda79c0a332cff03f6ae59e26c0d073b86e2a9
Ivan Lev <fanatos@mail.ru>
2023-12-17 01:07:57 +0500

    fix: update vite config for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/portfolio/',
   plugins: [react(), svgr()],
   build: {
     rollupOptions: {
