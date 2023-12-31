a4462519ea06cf5be339851527e6132560c4e77c
Ivan Lev <fanatos@mail.ru>
2023-12-31 19:15:26 +0500

    style: refactor Credits component

--- src/components/Credits/Credits.jsx
+++ src/components/Credits/Credits.jsx
@@ -1,75 +1,58 @@
-import React, { useContext } from 'react';
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import './Credits.scss';
 
 import { TranslationContext } from '../../contexts/translationContext.js';
-
-import logo from '../../images/favicon.png';
-import telegramIcon from '../../images/icon-social-telegram.svg';
 
 export default function Credits() {
   const translation = useContext(TranslationContext);
 
   return (
     <section className="credits">
-      <ul className="credits__list">
-        <li className="credits__list-element">
-          <span className="credits__text">{translation.creditsSite}</span>
-          <Link
-            className="credits__link"
-            to="https://www.figma.com/file/WrsiAQStgOWMzBWLzPHDAi/Developer-Portfolio-Design-(Community)?type=design&t=hRTNcG4J9JY8YIR6-6"
-          >
-            Figma
-          </Link>
-        </li>
-        <li className="credits__list-element">
-          <span className="credits__text">{translation.creditsLogo}</span>
-          <Link className="credits__link" to="https://www.flaticon.com/free-icon/coding_1159385">
-            Flaticon.com
-          </Link>
-          <img className="credits__image" src={logo}></img>
-        </li>
-        <li className="credits__list-element">
-          <span className="credits__text">{translation.creditsTelegramLogo}</span>
-          <Link className="credits__link" to="https://www.svgrepo.com/svg/378466/telegram-fill">
-            Svgrepo.com
-          </Link>
-          <img className="credits__image" src={telegramIcon}></img>
-        </li>
-        <li className="credits__list-element">
-          <span className="credits__text">{translation.creditsVitePlugin}</span>
-          <Link className="credits__link" to="https://www.npmjs.com/package/vite-plugin-svgr">
-            vite-plugin-svgr
-          </Link>
-        </li>
-        <li className="credits__list-element">
-          <span className="credits__text">{translation.creditsSunMoonIcons}</span>
-          <Link
-            className="credits__link"
-            to="https://www.svgrepo.com/collection/solar-outline-icons/"
-          >
-            Solar Outline Icons
-          </Link>
-        </li>
-        <li className="credits__list-element">
-          <span className="credits__text">{translation.creditsCssCustomProps}</span>
-          <Link
-            className="credits__link"
-            to="https://tproger.ru/articles/custom-properties-in-css-part-1"
-          >
-            Tproger
-          </Link>
-        </li>
-        <li className="credits__list-element">
-          <span className="credits__text">{translation.creditsBurgers}</span>
-          <Link
-            className="credits__link"
-            to="https://www.svgrepo.com/collection/nataicons-oval-tiny-line-icons/"
-          >
-             SvgRepo.com
-          </Link>
-        </li>
-      </ul>
+      <h2 className="title-gray">{translation.creditsTitle}</h2>
+      <p className="subtitle">{translation.creditsSubtitle}</p>
+
+      <p className="text">
+        <span className="credits__text">{translation.creditsSite}</span>
+        <Link
+          className="credits__link"
+          to="https://www.figma.com/file/WrsiAQStgOWMzBWLzPHDAi/Developer-Portfolio-Design-(Community)?type=design&t=hRTNcG4J9JY8YIR6-6"
+        >
+          Figma
+        </Link>
+
+        <span className="credits__text">{translation.creditsLogo}</span>
+        <Link className="credits__link" to="https://www.flaticon.com/free-icon/coding_1159385">
+          Flaticon.com
+        </Link>
+
+        <span className="text credits__text">{translation.creditsIcons}</span>
+        <Link className="credits__link" to="https://www.svgrepo.com/">
+          Svgrepo.com
+        </Link>
+
+        <span className="text credits__text">{translation.creditsSunMoonIcons}</span>
+        <Link
+          className="credits__link"
+          to="https://www.svgrepo.com/collection/solar-outline-icons/"
+        >
+          Solar Outline Icons
+        </Link>
+      </p>
+      <p className="text">
+        <span className="text credits__text">{translation.creditsVitePlugin}</span>
+        <Link className="credits__link" to="https://www.npmjs.com/package/vite-plugin-svgr">
+          vite-plugin-svgr
+        </Link>
+
+        <span className="text credits__text">{translation.creditsCssCustomProps}</span>
+        <Link
+          className="credits__link"
+          to="https://tproger.ru/articles/custom-properties-in-css-part-1"
+        >
+          Tproger
+        </Link>
+      </p>
     </section>
   );
 }
