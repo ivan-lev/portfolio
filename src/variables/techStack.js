4ce409a99c106c43fa5bde0585fdc2bea25c6e75
Ivan Lev <fanatos@mail.ru>
2023-12-31 19:20:53 +0500

    refactor: change structure of exported object

--- src/variables/techStack.js
+++ src/variables/techStack.js
@@ -1,46 +1,29 @@
-import sass from '../images/icon-tech-sass.svg';
-import css from '../images/icon-tech-css.svg';
-import git from '../images/icon-tech-git.svg';
-import github from '../images/icon-tech-github.svg';
-import html from '../images/icon-tech-html.svg';
-import js from '../images/icon-tech-js.svg';
-import nodejs from '../images/icon-tech-nodejs.svg';
-import react from '../images/icon-tech-react.svg';
-import redux from '../images/icon-tech-redux.svg';
-
+import css from '../assets/icons/tech/css.svg';
 import figma from '../assets/icons/tech/figma.svg';
+import git from '../assets/icons/tech/git.svg';
+import github from '../assets/icons/tech/github.svg';
+import html from '../assets/icons/tech/html.svg';
+import js from '../assets/icons/tech/js.svg';
 import mongo from '../assets/icons/tech/mongo.svg';
+import nodejs from '../assets/icons/tech/node.svg';
 import postman from '../assets/icons/tech/postman.svg';
+import reactjs from '../assets/icons/tech/reactjs.svg';
+import redux from '../assets/icons/tech/redux.svg';
+import sass from '../assets/icons/tech/sass.svg';
 import vscode from '../assets/icons/tech/vscode.svg';
 
-export const techStack = [
-  { logo: html, alt: 'HTML' },
-  { logo: css, alt: 'CSS' },
-  { logo: js, alt: 'JavaScript' },
-  { logo: figma, alt: 'Figma`' },
-  { logo: git, alt: 'Git' },
-  { logo: github, alt: 'Github' },
-  { logo: vscode, alt: 'VSCode`' },
-  { logo: sass, alt: 'SASS' },
-  { logo: react, alt: 'React' },
-  { logo: redux, alt: 'Redux' },
-  { logo: nodejs, alt: 'NodeJS' },
-  { logo: mongo, alt: 'MongoDB`' },
-  { logo: postman, alt: 'Postman' }
-];
-
-export const techStackNew = {
-  sass: { logo: sass, alt: 'SASS' },
-  css: { logo: css, alt: 'CSS' },
-  git: { logo: git, alt: 'Git' },
-  github: { logo: github, alt: 'Github' },
-  html: { logo: html, alt: 'HTML' },
-  js: { logo: js, alt: 'JavaScript' },
-  nodejs: { logo: nodejs, alt: 'NodeJS' },
-  react: { logo: react, alt: 'React' },
-  redux: { logo: redux, alt: 'Redux' },
-  postman: { logo: postman, alt: 'Postman' },
-  mongo: { logo: mongo, alt: 'MongoDB`' },
-  figma: { logo: figma, alt: 'Figma`' },
-  vscode: { logo: vscode, alt: 'VSCode`' }
+export const techStack = {
+  sass: { logo: sass, title: 'SASS' },
+  css: { logo: css, title: 'CSS' },
+  git: { logo: git, title: 'Git' },
+  github: { logo: github, title: 'Github' },
+  html: { logo: html, title: 'HTML' },
+  js: { logo: js, title: 'JavaScript' },
+  nodejs: { logo: nodejs, title: 'NodeJS' },
+  react: { logo: reactjs, title: 'React' },
+  redux: { logo: redux, title: 'Redux' },
+  postman: { logo: postman, title: 'Postman' },
+  mongo: { logo: mongo, title: 'MongoDB' },
+  figma: { logo: figma, title: 'Figma' },
+  vscode: { logo: vscode, title: 'VSCode' }
 };
