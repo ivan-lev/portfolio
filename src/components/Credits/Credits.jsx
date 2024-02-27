5f81e435d33856d069d0c164f275feba0273a93f
Ivan Lev <fanatos@mail.ru>
2024-02-27 16:08:13 +0500

    feat: add meshspin to credits

--- src/components/Credits/Credits.jsx
+++ src/components/Credits/Credits.jsx
@@ -1,18 +1,34 @@
-import { useContext } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './Credits.scss';
 
 import { TranslationContext } from '../../contexts/translationContext.js';
 
+import logoFigure from '../../vendor/meshspin';
+
 export default function Credits() {
   const translation = useContext(TranslationContext);
+
+  const [isMeshLoaded, setIsMeshLoaded] = useState(false);
+
+  useEffect(() => {
+    setIsMeshLoaded(true);
+  }, []);
+
+  useEffect(() => {
+    if (isMeshLoaded) {
+      logoFigure();
+    }
+  }, [isMeshLoaded]);
 
   return (
     <article className="credits">
       <h2 className="title-gray">{translation.creditsTitle}</h2>
       <p className="subtitle">{translation.creditsSubtitle}</p>
 
-      <div className="text">
+      <div className="mesh-spin" id="wrapper"></div>
+
+      <p className="text">
         <span className="credits__text">{translation.creditsSite}</span>
         <Link
           className="credits__link"
@@ -20,17 +36,23 @@ export default function Credits() {
         >
           Figma
         </Link>
+      </p>
 
+      <p className="text">
         <span className="credits__text">{translation.creditsLogo}</span>
         <Link className="credits__link" to="https://www.flaticon.com/free-icon/coding_1159385">
           Flaticon.com
         </Link>
+      </p>
 
+      <p className="text">
         <span className="credits__text">{translation.creditsIcons}</span>
         <Link className="credits__link" to="https://www.svgrepo.com/">
           Svgrepo.com
         </Link>
+      </p>
 
+      <p className="text">
         <span className="credits__text">{translation.creditsSunMoonIcons}</span>
         <Link
           className="credits__link"
@@ -38,20 +60,29 @@ export default function Credits() {
         >
           Solar Outline Icons
         </Link>
-      </div>
+      </p>
 
-      <div className="text">
+      <p className="text">
         <span className="credits__text">{translation.creditsVitePlugin}</span>
         <Link className="credits__link" to="https://www.npmjs.com/package/vite-plugin-svgr">
           vite-plugin-svgr
         </Link>
+      </p>
 
+      <p className="text">
         <span className="credits__text">{translation.creditsCssCustomProps}</span>
         <Link
           className="credits__link"
           to="https://tproger.ru/articles/custom-properties-in-css-part-1"
         >
           Tproger
+        </Link>
+      </p>
+
+      <div className="text">
+        <span className="credits__text">Вращающаяся фиговинка </span>
+        <Link className="credits__link" to="https://kickstart.ch/pages/meshspin-js.html">
+          meshspin-js
         </Link>
       </div>
     </article>
